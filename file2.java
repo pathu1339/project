7cdc7bde16ede7f05c2096e0b231d024c594b821
pathu1339 <93193204+pathu1339@users.noreply.github.com>
2021-11-22 15:54:37 +0530

    Update file2.java

--- file2.java
+++ file2.java
@@ -3,6 +3,12 @@ class DemoSingleArray2
 	public static void main(String args [])
 	{
 		int a[]={10,20,30,40,50,60};
+		//for loop
+		
+		for(int i=0; i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
 
 		
 		//for each
