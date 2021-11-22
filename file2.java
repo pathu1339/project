0d3356883c1d98dfa57588e0e332dcce57d3ba17
prathamesh <prathameshlohakare9@gmail.com>
2021-11-22 15:40:33 +0530

    this is fist commit

---
+++ file2.java
@@ -0,0 +1,14 @@
+class DemoSingleArray2
+{
+	public static void main(String args [])
+	{
+		int a[]={10,20,30,40,50,60};
+
+		
+		//for each
+		for(int i : a)
+		{
+			System.out.println(i);
+		}
+	}
+}
